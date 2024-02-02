0e6eb241489bc74a06e351a45df8e123930930d6
Michael Shustin <michaelshustin@gmail.com>
2024-02-03 00:26:14 +0200

    Added regex

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use git2::Repository;
-use git2::Delta;
-use clap::Parser;
-use std::path::PathBuf;
 use anyhow::{Context, Result};
-use log::{debug, info};
+use clap::Parser;
 use env_logger;
+use git2::Delta;
+use git2::Repository;
+use log::{debug, info};
+use std::path::PathBuf;
+use regex::Regex;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -21,35 +22,53 @@ struct Args {
 }
 
 fn main() -> Result<()> {
-
     env_logger::init();
     let args = Args::parse();
 
     let repo = Repository::open(args.path).context("Can't open repository")?;
-    let head = repo.head().context("Can't retreive repo head")?;
     let gitref = repo.resolve_reference_from_short_name(&args.gitref)?;
-    debug!("Repo head is {:?}", gitref.shorthand().unwrap());
-    let head_tree = head.peel_to_tree().context(format!("Unable to peel head: {:?}", head.name()))?;
-    let diff = repo.diff_tree_to_workdir_with_index(Some(&head_tree), None)?;
+    debug!(
+        "Repo head is '{}'",
+        gitref.name().expect("gitref is not UTF-8")
+    );
+    debug!("Repo head shortname '{}'", gitref.shorthand().unwrap());
+    let gitref_tree = gitref.peel_to_tree().context("Unable to peel gitref")?;
+    let diff = repo.diff_tree_to_workdir_with_index(Some(&gitref_tree), None)?;
 
     diff.foreach(
         &mut |file, _| {
-            println!("Analyzing '{}'", file.new_file().path().unwrap().to_str().unwrap());
+            info!(
+                "Analyzing '{}'",
+                file.new_file().path().unwrap().to_str().unwrap()
+            );
             true
         },
-        None,  // Ignore binary files
-        Some(&mut |file, hunk| {
-            match file.status() {
-                Delta::Modified => {
-                    println!("\tChanges in lines {}...{}", hunk.new_start(), hunk.new_lines());
-                    true
-                }
-                _ => true
+        None, // Ignore binary files
+        Some(&mut |file, hunk| match file.status() {
+            Delta::Modified => {
+                debug!(
+                    "Changes in lines {}..{}",
+                    hunk.new_start(),
+                    hunk.new_start() + hunk.new_lines()
+                );
+                true
             }
+            _ => true,
         }),
-        None,  // Extrapolating line information from hunks is enough, no need for line callback
-    ).context("Issue when iterating over diff")?;
+        None, // Extrapolating line information from hunks is enough, no need for line callback
+    )
+    .context("Issue when iterating over diff")?;
 
+    let regex_pattern = r#":(\d+)"#;
+    let regex = Regex::new(regex_pattern).expect("Failed to create regex");
+    let input_string = "cluster_config.py:481:4: C0116: Missing function or method docstring (missing-function-docstring)";
+
+    if let Some(captures) = regex.captures(input_string) {
+        if let Some(number_match) = captures.get(1) {
+            let number = number_match.as_str().parse::<u32>().unwrap();
+            println!("Found number: {}", number);
+        }
+    }
 
     Ok(())
 }
