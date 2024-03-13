a7c6a2cafc8aaa3b131ac8c7cf55c558f832cf8f
Michael Shustin <michaelshustin@gmail.com>
2024-03-13 10:27:48 +0200

    Support colorized output, better gitref handling

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ use env_logger;
 use env_logger::Env;
 use git2::Delta;
 use git2::Diff;
+use git2::DiffOptions;
 use git2::Repository;
 use log::{debug, info};
 use regex::Regex;
 use std::collections::HashMap;
 use std::io::{self, BufRead};
 use std::path::PathBuf;
+use std::process;
 
 type HunkRange = (u32, u32);
 
@@ -36,7 +38,7 @@ fn is_number_in_sorted_ranges(ranges: &[(u32, u32)], number: u32) -> bool {
         match (number >= ranges[mid].0, number <= ranges[mid].1) {
             (true, true) => return true,    // Number is within the current range
             (true, false) => low = mid + 1, // Number is greater than the current range, search in the right half
-            (false, _) => high = mid,       // Number is less than the current range, search in the left half
+            (false, _) => high = mid, // Number is less than the current range, search in the left half
         }
     }
 
@@ -44,14 +46,15 @@ fn is_number_in_sorted_ranges(ranges: &[(u32, u32)], number: u32) -> bool {
 }
 
 fn get_diff<'a>(repo: &'a Repository, gitref: &str) -> Result<Diff<'a>> {
-    let gitref = repo.resolve_reference_from_short_name(&gitref)?;
-    debug!(
-        "Repo head is '{}'",
-        gitref.name().expect("gitref is not UTF-8")
-    );
-    debug!("Repo head shortname '{}'", gitref.shorthand().unwrap());
-    let gitref_tree = gitref.peel_to_tree().context("Unable to peel gitref")?;
-    Ok(repo.diff_tree_to_workdir_with_index(Some(&gitref_tree), None)?)
+    let gitref = repo
+        .revparse_single(gitref)
+        .context("Unable to parse gitref")?;
+    let gitref_tree = gitref.peel_to_tree().context("Gitref is not a tree")?;
+    Ok(repo.diff_tree_to_workdir_with_index(
+        Some(&gitref_tree),
+        // Prevent errors on untouched lines by disabling context lines
+        Some(DiffOptions::new().context_lines(0)),
+    )?)
 }
 
 fn generate_hunkmap(diff: &Diff) -> Result<HashMap<String, Vec<HunkRange>>> {
@@ -84,6 +87,19 @@ fn generate_hunkmap(diff: &Diff) -> Result<HashMap<String, Vec<HunkRange>>> {
     Ok(hunkmap)
 }
 
+fn remove_ansi_colors(text: &str) -> String {
+    let re = Regex::new(r"\x1b\[[0-9;]*m").unwrap();
+    re.replace_all(text, "").to_string()
+}
+
+fn parse_lint_location(line: &str, regex: &Regex) -> Option<(String, u32)> {
+    regex.captures(line).and_then(|captures| {
+        let filename = captures.get(1)?.as_str().to_string();
+        let line_num = captures.get(2)?.as_str().parse().ok()?;
+        Some((filename, line_num))
+    })
+}
+
 fn main() -> Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("warn")).init();
     let args = Args::parse();
@@ -93,26 +109,42 @@ fn main() -> Result<()> {
 
     let file_hunks = generate_hunkmap(&diff)?;
 
-    debug!("{:?}", file_hunks);
+    let python_regex = Regex::new(r#"(.+?):(\d+)"#).expect("Failed to create python regex");
 
-    let regex = Regex::new(r#"(.+?):(\d+)"#).expect("Failed to create regex");
-
+    let mut failed = false;
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let line = line.expect("Could not read line from stdin");
 
-        if let Some(captures) = regex.captures(&line) {
-            let filename = captures.get(1).unwrap().as_str();
-            let line_num_match = captures.get(2).unwrap().as_str();
-
-            if let Some(hunk_ranges) = file_hunks.get(filename) {
-                let line_num = line_num_match.parse::<u32>().unwrap();
+        if let Some((filename, line_num)) =
+            parse_lint_location(&remove_ansi_colors(&line), &python_regex)
+        {
+            if let Some(hunk_ranges) = file_hunks.get(&filename) {
                 if is_number_in_sorted_ranges(hunk_ranges, line_num) {
                     println!("{}", line);
+                    failed = true;
                 }
             }
         }
     }
+    if failed {
+        process::exit(1);
+    } else {
+        Ok(())
+    }
+}
 
-    Ok(())
+#[cfg(test)]
+mod test {
+    use crate::parse_lint_location;
+    use regex::Regex;
+
+    #[test]
+    fn test_python_regex() {
+        let python_regex = Regex::new(r#"(.+?):(\d+)"#).expect("Failed to create python regex");
+        let (filename, line) =
+            parse_lint_location("pysrc/main.py:753:89: E501 Line too long", &python_regex).unwrap();
+        assert_eq!(filename, "pysrc/main.py");
+        assert_eq!(line, 753);
+    }
 }
